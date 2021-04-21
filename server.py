7eee280d02ed50f709f22bb350e737cb3102db90
Christian Rovet <Christian.rovet1@gmail.com>
2021-04-20 20:41:14 -0400

    initial

--- server.py
+++ server.py
@@ -11,14 +11,27 @@ EMAIL_REGEX= re.compile('^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 def index():
     return render_template("index.html")
 
-@app.route('/profile')
+@app.route('/recipes')
 def login():
-    query = "SELECT * FROM users WHERE id = %(id)s"
+    query = "SELECT * FROM users JOIN recipes ON users.id = user_id WHERE users.id = %(id)s"
+    query_user = "SELECT * FROM users WHERE users.id = %(id)s"
     data = {
         'id': session['id']
     }
-    user = connectToMySQL('login_registration').query_db(query,data)
-    return render_template('profile.html', user=user)
+    results = connectToMySQL('recipes').query_db(query,data)
+    user = connectToMySQL('recipes').query_db(query_user,data)
+    return render_template('recipes.html', recipes=results, user=user[0])
+
+@app.route('/show/<int:recipe_id>')
+def show_recipe(recipe_id):
+    if 'id' not in session:
+        return redirect('/')
+    query = "SELECT * FROM recipes WHERE id = %(id)s;"
+    data = {
+        'id': recipe_id
+    }
+    results = connectToMySQL('recipes').query_db(query,data)
+    return render_template('show.html', recipe=results[0])
 
 @app.route('/generate', methods=['post'])
 def generate():
@@ -64,7 +77,7 @@ def generate():
                 'email': email,
                 'password': pw_hash
             }
-            connectToMySQL('login_registration').query_db(query,data)
+            connectToMySQL('recipes').query_db(query,data)
             return redirect('/')
     # Login
     elif (request.form['type'] == 'login'):
@@ -80,7 +93,7 @@ def generate():
         data = {
             'email': request.form['email'],
         }
-        check = connectToMySQL('login_registration').query_db(query,data)
+        check = connectToMySQL('recipes').query_db(query,data)
         if len(check) < 1:
             flash("invalid email", "login")
             is_valid = False
@@ -89,8 +102,92 @@ def generate():
             is_valid = False
         if (is_valid):
             session['id'] = check[0]['id']
-            return redirect('/profile')
+            return redirect('/recipes')
     return redirect('/')
+
+@app.route('/new')
+def new():
+    if'id' not in session:
+        return redirect('/')
+    return render_template('new.html')
+
+
+@app.route('/create/recipe', methods=['post'])
+def create_recipe():
+    if 'id' not in session:
+        return redirect('/')
+    if not validate_recipe(request.form):
+        return redirect('/new')
+    query = 'INSERT INTO recipes (name,description,instructions,under_thirty,created_at,updated_at,user_id) VALUES (%(name)s,%(description)s,%(instructions)s,%(under_thirty)s, NOW(),NOW(),%(user_id)s);'
+    data = {
+        "name": request.form['name'],
+        "description": request.form['description'],
+        "instructions": request.form['instructions'],
+        "under_thirty": request.form['under_thirty'],
+        "user_id": session['id']
+    }
+    result = connectToMySQL('recipes').query_db(query,data)
+    return redirect('/recipes')
+
+@app.route('/edit/<int:recipe_id>')
+def edit_recipe(recipe_id):
+    if 'id' not in session:
+        return redirect('/')
+    query = "SELECT * FROM recipes WHERE id = %(id)s;"
+    data = {
+        'id': recipe_id
+    }
+    results = connectToMySQL('recipes').query_db(query,data)
+    return render_template('edit.html', recipe=results[0])
+
+@app.route('/delete/<int:recipe_id>')
+def delete_user(recipe_id):
+    if 'id' not in session:
+        return redirect('/')
+    query = "DELETE FROM recipes WHERE id = %(id)s;"
+    data = {
+        'id': recipe_id
+    }
+    results = connectToMySQL('recipes').query_db(query,data)
+    return redirect('/recipes')
+
+@app.route('/edit/recipe/<int:recipe_id>', methods=['post'])
+def update_recipe(recipe_id):
+    if 'id' not in session:
+        return redirect('/')
+    if not validate_recipe(request.form):
+        return redirect(f"/edit/{recipe_id}")
+    query = "UPDATE recipes SET name=%(name)s, description=%(description)s, instructions=%(instructions)s,under_thirty=%(under_thirty)s,user_id=%(user_id)s, updated_at=NOW() WHERE recipes.id = %(id)s;"
+    data ={
+        'id': recipe_id,
+        'name': request.form['name'],
+        'description': request.form['description'],
+        'instructions': request.form['instructions'],
+        'under_thirty': request.form['under_thirty'],
+        'user_id': request.form['user_id']
+    }
+
+    results = connectToMySQL('recipes').query_db(query,data)
+    return redirect(f"/show/{recipe_id}")
+
+def validate_recipe(recipe):
+    is_valid = True
+    if len(recipe['name']) < 3:
+        is_valid = False
+        flash('Recipe name must be at least 3 characters', "recipe")
+    if len(recipe['description']) < 3:
+        is_valid = False
+        flash('Recipe description must be at least 3 characters', "recipe")
+    if len(recipe['instructions']) < 3:
+        is_valid = False
+        flash('Recipe instructions must be at least 3 characters', "recipe")
+    return is_valid
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
